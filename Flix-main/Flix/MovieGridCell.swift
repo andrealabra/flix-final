f7a087cf89cfb6b697052c7aab0aca66b82a8f7e
Andrea Labra Orozco <andrealabra21@gmail.com>
2022-02-08 21:43:54 -0800

    Added flix

---
+++ Flix-main/Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Andrea Labra 
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
